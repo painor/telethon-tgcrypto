0633b71ca6910d05ddb6926751af633888f5fcf0
Unknown <iamkenzo404@gmail.com>
2022-04-08 12:04:03 +0700

    Bump version and up classifiers metadata requires

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='telethon-cryptg',
-    version='0.0.3',
+    version='0.0.4',
     description='telethon bindings for the tgcrypto module',
     long_description=open('README.rst').read().strip(),
     author='Painor',
@@ -17,10 +17,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
